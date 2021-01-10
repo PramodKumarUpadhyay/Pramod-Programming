dbb6c9d02d5ae10b06d33b3a9e6a70161ba7195f
PramodKumarUpadhyay <77151176+PramodKumarUpadhyay@users.noreply.github.com>
2021-01-10 12:51:54 +0530

    Add files via upload

---
+++ 1 - Basic Operator.R
@@ -0,0 +1,111 @@
+# Assigning Numneric Value to x & y#
+x <- 2
+x
+
+y <- a
+y
+
+# Assigning Character Value to x#
+x <- "IMS"
+x
+
+#Basic Maths#
+a <- 12
+b <- 15
+c <- 18
+
+#Addition
+a+b+c
+
+#Substraction
+a-b-c
+
+# Multiplication
+a*b*c
+
+#Division
+a/b/c
+
+# Arithmetic operator#
+a <- 91
+b <- 11
+
+#div
+a/b
+
+#Reminder
+a%%b
+
+#Relational less / more than equal to operator#
+p <- 75
+q <- 25
+r <- 3
+
+p <= q
+p <= q
+r <= q
+r > p
+
+# Relational not equal to operator#
+p != q
+r != r
+r == r
+p == q
+p == q*r
+
+p/q*r
+q*r/p
+
+# AND operator with z = 1 #
+z <- 1
+(z > 2) & (z > 7)
+
+# AND operator with z = 12 #
+z <- 12
+(z < 7) & (z > 15)
+(z > 7) & (z > 15)
+(z > 7) & (z < 15)
+
+# OR operator with z = 1 #
+z <- 1
+(z > 2) | (z > 7)
+(z > 2) | (z < 7)
+
+# AND operator with z = 12 #
+z <- 12
+(z < 7) | (z > 15)
+(z > 7) | (z > 15)
+(z > 7) | (z < 15)
+
+# Chnage the value and check what output you have #
+
+# Character Data Types #
+
+k <- "IMS"
+# print class of k #
+print(class(k))
+
+k <- 21.7
+# print class of k #
+print(class(k))
+
+k <- 25L
+# print class of k #
+print(class(k))
+
+k <- 7+8i
+# print class of k #
+print(class(k))
+
+k <- TRUE
+# print class of k #
+print(class(k))
+
+k <- 25D
+# print class of k #
+print(class(k))
+
+k <- 25/6
+# print class of k #
+print(class(k))
+
